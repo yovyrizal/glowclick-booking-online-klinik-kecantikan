153dc998882f5f0eaac5acd9b0b3d1d03f90bea7
yovyrizal <yovyrizal@gmail.com>
2026-05-09 13:01:42 +0700

    fix: fix salah link menuju ke css

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
 
   <!-- Main CSS -->
-  <link rel="stylesheet" href="./assets/css/style.css" />
+  <link rel="stylesheet" href="./assets/style/style.css" />
 </head>
 <body>
 
